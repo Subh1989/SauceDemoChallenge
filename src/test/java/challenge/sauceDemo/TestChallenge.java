113748e4bf02c7dd2c498c26f5123468d164017a
Subhankar <nsubhankar70@gmail.com>
2022-02-03 14:41:43 +0530

    Updated script

--- src/test/java/challenge/sauceDemo/TestChallenge.java
+++ src/test/java/challenge/sauceDemo/TestChallenge.java
@@ -15,28 +15,26 @@ public class TestChallenge {
 		
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://www.saucedemo.com/");
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
 		driver.findElement(By.id("password")).sendKeys("secret_sauce");
 		driver.findElement(By.id("login-button")).click();
 		List<WebElement>priceLists = driver.findElements(By.className("inventory_item_price"));
+		Double maximumPrice = Double.parseDouble(priceLists.get(0).getText().substring(1));
 		ArrayList<Double> finalItemPrices = new ArrayList<Double>();
 		for(int i=0; i<priceLists.size(); i++)
 		{
 			String prices = priceLists.get(i).getText();
 			Double itemPrice = Double.parseDouble(prices.substring(1));
 			finalItemPrices.add(itemPrice);
+			if(finalItemPrices.get(i)>maximumPrice) {
+				maximumPrice = finalItemPrices.get(i);
+				driver.findElements(By.xpath("//*[@class='inventory_item_price']/following-sibling::button")).get(i).click();
+				Assert.assertEquals(driver.findElements(By.xpath("//*[@class='inventory_item_price']/following-sibling::button")).get(i).getText(), "REMOVE");
+			}
 			
 		
-		}
-		Double maximumPrice = finalItemPrices.get(0);
-		for(int j=0; j<finalItemPrices.size(); j++)
-		{
-			if(finalItemPrices.get(j)>maximumPrice) {
-				maximumPrice = finalItemPrices.get(j);
-				driver.findElements(By.xpath("//*[@class='inventory_item_price']/following-sibling::button")).get(j).click();
-				Assert.assertEquals(driver.findElements(By.xpath("//*[@class='inventory_item_price']/following-sibling::button")).get(j).getText(), "REMOVE");
-			}
 		}
 		System.out.println("Maximum Price in the product lists: "+maximumPrice);
 		driver.findElement(By.id("shopping_cart_container")).click();
